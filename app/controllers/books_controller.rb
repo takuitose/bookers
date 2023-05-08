fd1a4fe4e10b498a18fa67198ca979c90df0ba71
takuitose <132122425+takuitose@users.noreply.github.com>
2023-05-08 14:02:03 +0000

    [Update]Bookers(form_with)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    list.save
+    redirect_to book_path(@book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -10,4 +18,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+private
+def book_params
+  params.require(:book).permit(:title,:body)
+end
+
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,11 +1,6 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get books_new_url
-    assert_response :success
-  end
-
   test "should get index" do
     get books_index_url
     assert_response :success
