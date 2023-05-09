69922c8f7b7d07296cf02d519153e2e2f00d505c
takuitose <132122425+takuitose@users.noreply.github.com>
2023-05-09 04:52:48 +0000

    [Update]bookers(show,edit)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    list.save
+    @book.save
     redirect_to book_path(@book.id)
   end
 
@@ -14,9 +14,23 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
 private
